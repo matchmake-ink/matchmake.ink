42ba382b05721b2de63a31300e461196d238d036
firesquid <jdeiss06@gmail.com>
2023-06-16 15:48:06 -0500

    add header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: {
+          main: "#07020d",
+          lighter: "#2c174a",
+          lightest: "#321659",
+        },
+        primary: {
+          main: "#eaf4f4",
+          darker: "#daeded",
+          darkest: "#b4dadb",
+        },
+        accent: {
+          50: "#f0fdf5",
+          100: "#dcfce9",
+          200: "#bbf7d3",
+          300: "#86efb2",
+          400: "#4ade88",
+          500: "#23ce6b",
+          600: "#16a351",
+          700: "#158043",
+          800: "#166538",
+          900: "#145330",
+          950: "#052e18",
+        },
       },
     },
   },
   plugins: [],
-}
+};
