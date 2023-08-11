5bb16bb27ad0e95339cb9df3f8bf0e9cf54ab28b
firesquid <jdeiss06@gmail.com>
2023-08-11 14:26:58 -0500

    change workflow name

--- components/button.tsx
+++ components/button.tsx
@@ -5,7 +5,6 @@ export interface ButtonProps {
   onClick: () => void;
 }
 export default function Button({ label, color, onClick }: ButtonProps) {
-  console.log(color);
   return (
     <button
       className={`bg-primary-500 hover:bg-primary-400 text-text font-bold py-2 px-4 focus:outline-none focus:shadow-outline transition-all rounded-lg outline-none`}
