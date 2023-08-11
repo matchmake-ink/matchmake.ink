fc77229ea47e3ef75ed205dd61bddd049370e0b1
firesquid <jdeiss06@gmail.com>
2023-08-10 20:56:50 -0500

    add header

---
+++ components/header.tsx
@@ -0,0 +1,23 @@
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between px-4 py-2 bg-background-dark border-text border-b h-20">
+      <h1 className="font-stylized opacity-0 w-0 lg:opacity-100 lg:w-full">
+        matchmake.ink
+      </h1>
+      <nav className="w-full flex lg:justify-center">
+        <a className="nav-item" href="/play">
+          Play
+        </a>
+        <a className="nav-item" href="/tournaments">
+          Tournaments
+        </a>
+        <a className="nav-item" href="/leaderboard">
+          Leaderboard
+        </a>
+        <a className="nav-item" href="/settings">
+          Settings
+        </a>
+      </nav>
+    </header>
+  );
+}
