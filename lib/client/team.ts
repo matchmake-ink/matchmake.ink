eea14d04808ac1afe96812c162aad1da2f817f60
firesquid <jdeiss06@gmail.com>
2023-08-14 15:15:51 -0500

    add useTeam hook

--- lib/client/team.ts
+++ lib/client/team.ts
@@ -1,6 +1,16 @@
 import { auth } from "@/lib/client/firebase";
+import { doc } from "firebase/firestore";
+import { db } from "@/lib/client/firebase";
+import { useProfile } from "./profile";
+import { useDocument } from "react-firebase-hooks/firestore";
 
 // note - this file does not have unit tests because it simply wraps api calls
+
+export interface Team {
+  id: string;
+  name: string;
+  members: string[];
+}
 
 export async function createTeam(teamName: string): Promise<void> {
   const res = await fetch("/api/team/create", {
@@ -27,4 +37,31 @@ export async function createTeam(teamName: string): Promise<void> {
     return Promise.reject();
   }
   return Promise.resolve();
+}
+
+export function useTeam() {
+  const { profile, profileLoading, profileError } = useProfile();
+  const [value, loading, error] = useDocument(
+    doc(db, `teams/${profile?.teamId || "freeAgent"}`)
+  );
+
+  if (profile === undefined && !profileLoading) {
+    return {
+      team: undefined,
+      teamLoading: false,
+      teamError: profileError,
+    };
+  }
+
+  const team: Team = {
+    id: value?.data()?.id,
+    name: value?.data()?.name,
+    members: value?.data()?.members,
+  };
+
+  return {
+    team,
+    teamLoading: loading,
+    teamError: error,
+  };
 }
