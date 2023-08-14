01ffbb88b87a0d0fa2cf121831e08bb09810c079
firesquid <jdeiss06@gmail.com>
2023-08-14 10:16:19 -0500

    make sidebar pass tests

---
+++ components/sidebar.tsx
@@ -0,0 +1,10 @@
+export default function Sidebar() {
+  return (
+    <nav>
+      <a href="/play">Home</a>
+      <a href="/team">Team</a>
+      <a href="/profile">Profile</a>
+      <a href="/danger">Danger Zone</a>
+    </nav>
+  );
+}
