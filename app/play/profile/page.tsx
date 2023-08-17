b2ef92d8403e71ab195c713af8b9367b0233d8a1
firesquid <jdeiss06@gmail.com>
2023-08-16 20:02:06 -0500

    add user's profile page

---
+++ app/play/profile/page.tsx
@@ -0,0 +1,11 @@
+import UserProfile from "@/components/user-profile";
+import ProfileEditor from "@/components/profile-editor";
+
+export default function ProfilePage() {
+  return (
+    <>
+      <UserProfile />
+      <ProfileEditor />
+    </>
+  );
+}
