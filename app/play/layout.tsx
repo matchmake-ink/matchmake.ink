03b4cd890b2f8bca1fc92e38a2c651754db5fb74
firesquid <jdeiss06@gmail.com>
2023-08-18 18:03:58 -0500

    make play a protected route

--- app/play/layout.tsx
+++ app/play/layout.tsx
@@ -1,3 +1,7 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useUser } from "@/lib/client/auth";
 import Sidebar from "@/components/sidebar";
 
 export default function PlayLayout({
@@ -5,6 +9,17 @@ export default function PlayLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const { user, userLoading, userError } = useUser();
+
+  useEffect(() => {
+    console.log(user, userLoading, userError);
+    if (userLoading) return;
+
+    if (user === null || userError) {
+      router.push("/auth");
+    }
+  }, [router, userLoading, user, userError]);
   return (
     <div className="flex flex-row">
       <Sidebar />
