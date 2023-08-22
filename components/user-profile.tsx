788b897e28c37f73397a053269da306061cd05d7
firesquid <jdeiss06@gmail.com>
2023-08-22 12:18:44 -0500

    add sharable link to profile page

--- components/user-profile.tsx
+++ components/user-profile.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { useProfile } from "@/lib/client/profile";
+import { useUser } from "@/lib/client/auth";
 import ProfileCard from "./profile-card";
+import Button from "./button";
 
 // retuns an automatically updating ProfileCard for the currently logged in User's profile
 export default function UserProfile() {
+  const { user } = useUser();
   const { profile, profileLoading, profileError } = useProfile();
 
   if (profileLoading) return <p>Loading...</p>;
@@ -11,18 +14,30 @@ export default function UserProfile() {
   return (
     <>
       {profile && !profileError ? (
-        <ProfileCard
-          // placeholder for avatar
-          avatarUrl={profile.avatar || "/images/user_placeholder.png"}
-          ign={profile.ign}
-          discordTag={profile.discordTag}
-          bio={profile.bio}
-          region={profile.region}
-          sendou={profile.sendou}
-          rank={profile.rank}
-          teamName="Bread Gang"
-          teamAvatar="/images/team_placeholder.png"
-        />
+        <>
+          <ProfileCard
+            // placeholder for avatar
+            avatarUrl={profile.avatar || "/images/user_placeholder.png"}
+            ign={profile.ign}
+            discordTag={profile.discordTag}
+            bio={profile.bio}
+            region={profile.region}
+            sendou={profile.sendou}
+            rank={profile.rank}
+            teamName="Bread Gang"
+            teamAvatar="/images/team_placeholder.png"
+          />
+          <Button
+            label="Copy Sharable Link"
+            color="primary"
+            className="my-4 max-w-lg mx-auto"
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `https://matchmake.ink/profile/${user?.uid}`
+              );
+            }}
+          />
+        </>
       ) : (
         <p>Something went wrong! Are you signed in?</p>
       )}
